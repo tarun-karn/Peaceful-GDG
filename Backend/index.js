2e2dcfb7d6af3e64a9382e5803e502ea658f4d8f
Tarun Karn <tarunkarnofficial@gmail.com>
2026-01-09 12:37:59 +0530

    database update

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const path = require("path");
